d79968346cfdd17302ca243fe0b419cfb9039170
DirectorLord <thelord161203@gmail.com>
2025-07-09 10:50:41 +0300

    solved question 2 (copy pasated)

--- OOP02/Program.cs
+++ OOP02/Program.cs
@@ -1,12 +1,15 @@
 ﻿namespace OOP02
 {
     using System;
+    using System.Runtime.CompilerServices;
+
     public class Program
     {
         #region Question3
         //Create an array of Employees with size three a DBA, Guest and the third one is security officer who have full permissions. (Employee [] EmpArr;)
         public string[] securityPrivileges = { "Guest", "DBA", "Secuirty officer" };
         #endregion
+
         #region Question1
         //Design and implement a Class for the employees in a company:
         public class Employee
@@ -65,8 +68,40 @@
             
 
         }
+        #endregion
+
+        #region Question2
+        //Develop a Class to represent the Hiring Date Data
+        public class hiringDate
+        {
+            private int day; private int month; private int year;
+            //the constructor
+            public hiringDate(int day, int month, int year)
+            {
+                this.day = day;
+                this.month = month;
+                this.year = year;
+            }
+            #region Setters and getters for date
+            #region day
+            public int getDay() { return day; }
+            public void setDay(int day) { this.day = day; }
             #endregion
 
+            #region month
+            public int getMonth() { return month; }
+            public void setMonth(int month) { this.month = month; }
+            #endregion
+
+            #region year
+            public int getYear() { return year; }
+            public void setYear(int year) { this.year = year; }
+            #endregion
+
+            #endregion
+
+        }
+        #endregion
         public static void Main(string[] args)
         {
 
