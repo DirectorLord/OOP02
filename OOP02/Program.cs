1ec971ee44a082efc52648de8cbc959efc905177
DirectorLord <thelord161203@gmail.com>
2025-07-09 08:39:21 +0300

    Add project files.

---
+++ OOP02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOP02
+{
+    using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
